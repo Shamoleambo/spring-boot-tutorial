c73bab41e4651a6294d97167747e04e7d3cf007f
Tiago Gomes <tidgomes@gmail.com>
2021-12-16 10:25:25 -0300

    Creation of the Service Layer for Student, which holds the business logic

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -12,16 +12,15 @@ import java.util.List;
 @RequestMapping(path="api/v1/student")
 public class StudentController {
 
-    @GetMapping
-    public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                    1L,
-                    "Mano Jalil",
-                    "mano@gmail.com",
-                    LocalDate.of(1994, Month.JULY, 17),
-                    27
-                )
-        );
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
     }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
 }
